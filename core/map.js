c8bc559ab6ebf2299e779be11d72142acedb426a
Daniel Beilinson <beilinson@gmail.com>
2013-08-30 01:58:29 +0400

    misc

--- core/map.js
+++ core/map.js
@@ -1,13 +1,18 @@
 var map = L.map('map'),
 		markerSize = 20,
-		iconsPath = "/images/map/";
+		iconsPath = "/images/map/",
+		iframe = false;
+
+if (window.location != window.parent.location) iframe = true;
+
+var attribution = iframe ? '<a class="logo" href="http://closedsociety.org" target="_blank">ClosedSociety.org</a><br />По данным <a href="http://closedsociety.org" target="_blank">ИАИ "Закрытое общество"</a>.' : '<a class="logo" href="http://www.svoboda.org/section/authorities-against-ngo/3228.html" target="_blank"><img src="../assets/img/liberty.png" /></a><br />Карта проверок - совместный проект<br />с <a href="http://www.svoboda.org/section/authorities-against-ngo/3228.html" target="_blank">радиостанцией "Свобода"</a>.';
 
 map.spin(true);
 
 map.addLayer(L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'))
 	.setView([55, 50], 5);
 
-map.attributionControl.setPrefix('').addAttribution('<a class="logo" href="http://closedsociety.org" target="_blank">ClosedSociety.org</a><br />По данным <a href="http://closedsociety.org" target="_blank">ИАИ "Закрытое общество"</a>.');
+map.attributionControl.setPrefix('');
 
 L.spriteIcon = function(marker) {
 	var color = '';
@@ -87,11 +92,101 @@ var markers = L.markerClusterGroup({
 	}*/
 });
 		
+var legendposition = iframe ? 'bottomleft' : 'bottomright',
+		legend = L.control({position:legendposition});
+
+legend.onAdd = function (map) {
+	this._div = L.DomUtil.create('div', 'legend');
+	this._div.innerHTML = '<ul id="legend"> \
+		<li><span class="red"></span> - деятельность НКО приостановлена</li> \
+		<li><span class="violet"></span> - возбуждено административное дело</li> \
+		<li><span class="orange"></span> - НКО получившие предостережения</li> \
+		<li><span class="yellow"></span> - НКО получившие представления</li> \
+		<li><span class="green"></span> - иные методы давления</li> \
+		<li><span class="blue"></span> - нет информации</li> \
+		</ul>';
+	return this._div;
+};
+
+legend.addTo(map);
+		
+var ShareControl = L.Control.extend({
+	options: {
+  	position: iframe ? 'topleft' : 'bottomright',
+  	embed: '',
+    url: '',
+    text: ''
+	},
+	initialize: function(_, options) {
+		L.setOptions(this, options);
+	},
+	onAdd: function(map) {
+		this._map = map;
+		var container = L.DomUtil.create('div', 'leaflet-control-share leaflet-bar');
+		var link = L.DomUtil.create('a', 'share icon icon-share', container);
+		link.href = '#';
+		L.DomEvent.addListener(link, 'click', this._shareClick, this);
+		L.DomEvent.disableClickPropagation(container);
+		this._map.on('mousedown', this._clickOut, this);
+		return container;
+	},
+	_clickOut: function(e) {
+		if (this._popup) {
+			this._map.removeLayer(this._popup);
+			this._popup = null;
+			return;
+		}
+	},
+	_shareClick: function(e) {
+		L.DomEvent.stop(e);
+		if (this._popup) return this._clickOut(e);
+		var url = this.options.url || 'http://closedsociety.org',
+				text = this.options.text || 'Monitor pressure on Russian NGOs',
+				embed = this.options.embed || 'http://closedsociety.org/map',
+				twitter = 'http://twitter.com/intent/tweet?status=' + encodeURIComponent(text + '\n' + url),
+				facebook = 'https://www.facebook.com/sharer.php?u=' + encodeURIComponent(url) + '&t=' + encodeURIComponent(text),
+				share = "<a class='leaflet-popup-close-button' href='#close'>×</a>" +
+					"<h3>Поделиться этой картой</h3>" + "<div class='share-buttons'><a class='share-facebook icon icon-facebook' target='_blank' href='" + 
+					facebook + "'>Facebook</a>" + "<a class='share-twitter icon icon-twitter' target='_blank' href='" + 
+					twitter + "'>Twitter</a></div>" + "<h3>Получить embed код</h3>" + "<small>Скопирйте и вставьте этот HTML код на вашу страницу.</small>" +
+          "<textarea rows=4>&lt;iframe width='500' height='300' frameBorder='0' src='" + embed + "'&gt;&lt;/iframe&gt;</textarea>";
+		this._popup = L.marker(this._map.getCenter(), {
+			zIndexOffset: 10000,
+			icon: L.divIcon({
+				className: 'share-popup',
+				iconSize: L.point(360, 240),
+				iconAnchor: L.point(180, 120),
+				html: share
+			})
+    })
+		.on('mousedown', function(e) {
+			L.DomEvent.stopPropagation(e.originalEvent);
+		})
+		.on('click', clickPopup, this).addTo(this._map);
+		function clickPopup(e) {
+			if (e.originalEvent && e.originalEvent.target.nodeName === 'TEXTAREA') {
+				var target = e.originalEvent.target;
+				target.focus();
+				target.select();
+			} else if (e.originalEvent && e.originalEvent.target.getAttribute('href') === '#close') {
+				this._clickOut(e);
+			}
+		}
+	}
+});
+
+var share = new ShareControl();
+share.addTo(map);
+
 L.control.fullscreen({
-	position: 'topleft',
+	position: iframe ? 'topleft' : 'bottomright',
 	title: 'Полноэкранная версия'
 }).addTo(map);
-			
+
+new L.Control.Attribution({
+	position: iframe ? 'bottomright' : 'bottomleft',
+}).setPrefix('').addAttribution(attribution).addTo(map);
+
 var info = L.control();
 
 info.onAdd = function (map) {
@@ -131,91 +226,6 @@ info.update = function (props) {
 };
 
 info.addTo(map);
-
-var legend = L.control({position:'bottomleft'});
-
-legend.onAdd = function (map) {
-	this._div = L.DomUtil.create('div', 'legend');
-	this._div.innerHTML = '<ul id="legend"> \
-		<li><span class="red"></span> - деятельность НКО приостановлена</li> \
-		<li><span class="violet"></span> - возбуждено административное дело</li> \
-		<li><span class="orange"></span> - НКО получившие предостережения</li> \
-		<li><span class="yellow"></span> - НКО получившие представления</li> \
-		<li><span class="green"></span> - иные методы давления</li> \
-		<li><span class="blue"></span> - нет информации</li> \
-		</ul>';
-	return this._div;
-};
-
-legend.addTo(map);
-
-var ShareControl = L.Control.extend({
-	options: {
-  	position: 'topleft',
-  	embed: '',
-    url: '',
-    text: ''
-	},
-	initialize: function(_, options) {
-		L.setOptions(this, options);
-	},
-	onAdd: function(map) {
-		this._map = map;
-		var container = L.DomUtil.create('div', 'leaflet-control-share leaflet-bar');
-		var link = L.DomUtil.create('a', 'share icon icon-share', container);
-		link.href = '#';
-		L.DomEvent.addListener(link, 'click', this._shareClick, this);
-		L.DomEvent.disableClickPropagation(container);
-		this._map.on('mousedown', this._clickOut, this);
-		return container;
-	},
-	_clickOut: function(e) {
-		if (this._popup) {
-			this._map.removeLayer(this._popup);
-			this._popup = null;
-			return;
-		}
-	},
-	_shareClick: function(e) {
-		L.DomEvent.stop(e);
-		if (this._popup) return this._clickOut(e);
-		var url = this.options.url || 'http://closedsociety.org',
-				text = this.options.text || 'Monitor pressure on Russian NGOs',
-				embed = this.options.embed || 'http://closedsociety.org/map.html',
-				twitter = 'http://twitter.com/intent/tweet?status=' + encodeURIComponent(text + '\n' + url),
-				facebook = 'https://www.facebook.com/sharer.php?u=' + encodeURIComponent(url) + '&t=' + encodeURIComponent(text),
-				share = "<a class='leaflet-popup-close-button' href='#close'>×</a>" +
-					"<h3>Поделиться этой картой</h3>" + "<div class='share-buttons'><a class='share-facebook icon icon-facebook' target='_blank' href='" + 
-					facebook + "'>Facebook</a>" + "<a class='share-twitter icon icon-twitter' target='_blank' href='" + 
-					twitter + "'>Twitter</a></div>" + "<h3>Получить embed код</h3>" + "<small>Скопирйте и вставьте этот HTML код на вашу страницу.</small>" +
-          "<textarea rows=4>&lt;iframe width='500' height='300' frameBorder='0' src='" + embed + "'&gt;&lt;/iframe&gt;</textarea>";
-		this._popup = L.marker(this._map.getCenter(), {
-			zIndexOffset: 10000,
-			icon: L.divIcon({
-				className: 'share-popup',
-				iconSize: L.point(360, 240),
-				iconAnchor: L.point(180, 120),
-				html: share
-			})
-    })
-		.on('mousedown', function(e) {
-			L.DomEvent.stopPropagation(e.originalEvent);
-		})
-		.on('click', clickPopup, this).addTo(this._map);
-		function clickPopup(e) {
-			if (e.originalEvent && e.originalEvent.target.nodeName === 'TEXTAREA') {
-				var target = e.originalEvent.target;
-				target.focus();
-				target.select();
-			} else if (e.originalEvent && e.originalEvent.target.getAttribute('href') === '#close') {
-				this._clickOut(e);
-			}
-		}
-	}
-});
-
-var share = new ShareControl();
-share.addTo(map);
 
 var geoJsonLayer = L.geoJson.ajax("http://closedsociety.org/api/geo/ncos",{dataType:"json"});
 geoJsonLayer.on('data:loaded',handleGeoJSON);
